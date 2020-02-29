3cb8b7bcd507439781cca1cc2c2e53a3ea019f0d
Rafael Delboni <rafadelboni@gmail.com>
2020-02-29 20:08:20 -0300

    feat: Adds tileset maker and level
    Stoped att TileMaps

---
+++ escape-the-maze/items/Pickup.gd
@@ -0,0 +1,35 @@
+extends Area2D
+
+var textures = {'coin': 'res://assets/coin.png',
+                'key_red': 'res://assets/keyRed.png',
+                'star': 'res://assets/star.png'}
+
+var type
+
+func _ready():
+  $Tween.interpolate_property($Sprite,
+                              'scale',
+                              Vector2(1, 1),
+                              Vector2(3, 3),
+                              0.5,
+                              Tween.TRANS_QUAD,
+                              Tween.EASE_IN_OUT)
+  $Tween.interpolate_property($Sprite,
+                              'modulate',
+                              Color(1, 1, 1, 1),
+                              Color(1, 1, 1, 0),
+                              0.5,
+                              Tween.TRANS_QUAD,
+                              Tween.EASE_IN_OUT)
+
+func init(_type, pos):
+  $Sprite.texture = load(textures[_type])
+  type = _type
+  position = pos
+
+func pickup():
+  $CollisionShape2D.disabled = true
+  $Tween.start()
+
+func _on_Tween_tween_completed(_object, _key):
+  queue_free()
