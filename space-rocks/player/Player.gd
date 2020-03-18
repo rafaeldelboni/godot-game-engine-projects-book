78a5657c50adccdded4ede129e015997a211ca04
Rafael Delboni <rafadelboni@gmail.com>
2020-03-17 21:26:49 -0300

    feat: Adds main level and start creating rocks
    Stopped at Variable size

--- space-rocks/player/Player.gd
+++ space-rocks/player/Player.gd
@@ -1,13 +1,22 @@
 extends RigidBody2D
 
+signal shoot
+
+export (PackedScene) var Bullet
+export (float) var fire_rate
+
 export (int) var engine_power
 export (int) var spin_power
 
 var thrust = Vector2()
 var rotation_dir = 0
 
+var can_shoot = true
+
 enum {INIT, ALIVE, INVULNERABLE, DEAD}
 var state = null
+
+var screensize = Vector2()
 
 func change_state(new_state):
   match new_state:
@@ -21,6 +30,13 @@ func change_state(new_state):
       $CollisionShape2D.disabled = true
   state = new_state
 
+func shoot():
+  if state == INVULNERABLE:
+    return
+  emit_signal("shoot", Bullet, $Muzzle.global_position, rotation)
+  can_shoot = false
+  $GunTimer.start()
+
 func get_input():
   thrust = Vector2()
   if state in [DEAD, INIT]:
@@ -32,13 +48,30 @@ func get_input():
     rotation_dir += 1
   if Input.is_action_pressed("rotate_left"):
     rotation_dir -= 1
+  if Input.is_action_pressed("shoot") and can_shoot:
+    shoot()
 
-func _physics_process(_delta):
-  set_applied_force(thrust.rotated(rotation))
-  set_applied_torque(spin_power * rotation_dir)
+func _ready():
+  screensize = get_viewport().get_visible_rect().size
+  change_state(ALIVE)
+  $GunTimer.wait_time = fire_rate
 
 func _process(_delta):
   get_input()
 
-func _ready():
-  change_state(ALIVE)
+func _integrate_forces(physics_state):
+  set_applied_force(thrust.rotated(rotation))
+  set_applied_torque(spin_power * rotation_dir)
+  var xform = physics_state.get_transform()
+  if xform.origin.x > screensize.x:
+    xform.origin.x = 0
+  if xform.origin.x < 0:
+    xform.origin.x = screensize.x
+  if xform.origin.y > screensize.y:
+    xform.origin.y = 0
+  if xform.origin.y < 0:
+    xform.origin.y = screensize.y
+  physics_state.set_transform(xform)
+
+func _on_GunTimer_timeout():
+  can_shoot = true
